0619c976ead080a1b72ca2f747df7a26644c0ea6
NonnoNanni7 <60818246+NonnoNanni7@users.noreply.github.com>
2020-02-08 16:18:15 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -18,49 +18,13 @@ $date = isset($message['date']) ? $message['date'] : "";
 $response = '';
 $leccata = [];
 
-if(isset($message['leccata']))
+if(isset($message['text']) && $message['text'] == 'leccata' || $message ['text'] == 'Leccata' || $message ['text'] == 'LECCATA')
 {
-	$response = "Hai leccato" . 2 . "volte!";
+	$response = "Hai leccato" . $message['text'] . "volte!";
 }
-// elseif(isset($message['audio']))
-// {
-// 	$response = "Ho ricevuto un messaggio audio";
-// }
-// elseif(isset($message['document']))
-// {
-// 	$response = "Ho ricevuto un messaggio document";
-// }
-// elseif(isset($message['photo']))
-// {
-// 	$response = "Ho ricevuto un messaggio photo";
-// }
-// elseif(isset($message['sticker']))
-// {
-// 	$response = "Ho ricevuto un messaggio sticker";
-// }
-// elseif(isset($message['video']))
-// {
-// 	$response = "Ho ricevuto un messaggio video";
-// }
-// elseif(isset($message['voice']))
-// {
-// 	$response = "Ho ricevuto un messaggio voice";
-// }
-// elseif(isset($message['contact']))
-// {
-// 	$response = "Ho ricevuto un messaggio contact";
-// }
-// elseif(isset($message['location']))
-// {
-// 	$response = "Ho ricevuto un messaggio location";
-// }
-// elseif(isset($message['venue']))
-// {
-// 	$response = "Ho ricevuto un messaggio venue";
-// }
 else
 {
-	$response = "Ho ricevuto un messaggio porcodio ?";
+	$response = "Ho ricevuto un messaggio porcoddio ?";
 }
 
 header("Content-Type: application/json");
