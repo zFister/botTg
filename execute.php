31ead73375a1c36800124735f5e56f0f3e22a363
NonnoNanni7 <60818246+NonnoNanni7@users.noreply.github.com>
2020-02-08 16:06:20 +0100

    Add files via upload

---
+++ execute.php
@@ -0,0 +1,69 @@
+<?php
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+if(!$update)
+{
+  exit;
+}
+
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+
+$response = '';
+$leccata = [];
+
+if(isset($message['leccata']))
+{
+	$response = "Hai leccato" . 2 . "volte!";
+}
+elseif(isset($message['audio']))
+{
+	$response = "Ho ricevuto un messaggio audio";
+}
+elseif(isset($message['document']))
+{
+	$response = "Ho ricevuto un messaggio document";
+}
+elseif(isset($message['photo']))
+{
+	$response = "Ho ricevuto un messaggio photo";
+}
+elseif(isset($message['sticker']))
+{
+	$response = "Ho ricevuto un messaggio sticker";
+}
+elseif(isset($message['video']))
+{
+	$response = "Ho ricevuto un messaggio video";
+}
+elseif(isset($message['voice']))
+{
+	$response = "Ho ricevuto un messaggio voice";
+}
+elseif(isset($message['contact']))
+{
+	$response = "Ho ricevuto un messaggio contact";
+}
+elseif(isset($message['location']))
+{
+	$response = "Ho ricevuto un messaggio location";
+}
+elseif(isset($message['venue']))
+{
+	$response = "Ho ricevuto un messaggio venue";
+}
+else
+{
+	$response = "Ho ricevuto un messaggio ?";
+}
+
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
