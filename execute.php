5ae458717961ad041aed2d36472cb098a799c3ec
NonnoNanni7 <60818246+NonnoNanni7@users.noreply.github.com>
2020-02-08 16:10:20 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -7,7 +7,7 @@ if(!$update)
   exit;
 }
 
-$message = isset($update['message']) ? $update['message'] : "";
+$message = isset($update['leccata']) ? $update['leccata'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
@@ -22,42 +22,42 @@ if(isset($message['leccata']))
 {
 	$response = "Hai leccato" . 2 . "volte!";
 }
-elseif(isset($message['audio']))
-{
-	$response = "Ho ricevuto un messaggio audio";
-}
-elseif(isset($message['document']))
-{
-	$response = "Ho ricevuto un messaggio document";
-}
-elseif(isset($message['photo']))
-{
-	$response = "Ho ricevuto un messaggio photo";
-}
-elseif(isset($message['sticker']))
-{
-	$response = "Ho ricevuto un messaggio sticker";
-}
-elseif(isset($message['video']))
-{
-	$response = "Ho ricevuto un messaggio video";
-}
-elseif(isset($message['voice']))
-{
-	$response = "Ho ricevuto un messaggio voice";
-}
-elseif(isset($message['contact']))
-{
-	$response = "Ho ricevuto un messaggio contact";
-}
-elseif(isset($message['location']))
-{
-	$response = "Ho ricevuto un messaggio location";
-}
-elseif(isset($message['venue']))
-{
-	$response = "Ho ricevuto un messaggio venue";
-}
+// elseif(isset($message['audio']))
+// {
+// 	$response = "Ho ricevuto un messaggio audio";
+// }
+// elseif(isset($message['document']))
+// {
+// 	$response = "Ho ricevuto un messaggio document";
+// }
+// elseif(isset($message['photo']))
+// {
+// 	$response = "Ho ricevuto un messaggio photo";
+// }
+// elseif(isset($message['sticker']))
+// {
+// 	$response = "Ho ricevuto un messaggio sticker";
+// }
+// elseif(isset($message['video']))
+// {
+// 	$response = "Ho ricevuto un messaggio video";
+// }
+// elseif(isset($message['voice']))
+// {
+// 	$response = "Ho ricevuto un messaggio voice";
+// }
+// elseif(isset($message['contact']))
+// {
+// 	$response = "Ho ricevuto un messaggio contact";
+// }
+// elseif(isset($message['location']))
+// {
+// 	$response = "Ho ricevuto un messaggio location";
+// }
+// elseif(isset($message['venue']))
+// {
+// 	$response = "Ho ricevuto un messaggio venue";
+// }
 else
 {
 	$response = "Ho ricevuto un messaggio ?";
